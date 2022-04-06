5c127c70eed84f4f6f18f7ff30e852aa2161c995
gainnam <gain1113@icloud.com>
2022-04-06 21:57:47 +0900

    First commit

---
+++ Algorithm/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module algorithm {
+	exports com.example.demo;
+
+	requires spring.beans;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.core;
+}
